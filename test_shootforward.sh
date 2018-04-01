0b5e1362c18c4aed5f33b61c18a930dc956aa813
JiancongWang <10811130d@gmail.com>
2018-04-01 19:20:16 -0400

    Update test_shootforward.sh

--- test_shootforward.sh
+++ test_shootforward.sh
@@ -1,2 +1,2 @@
-'/home/jiancong/Desktop/cuda-workspace/PointSetGeodesicShooting_CUDA/shoot_forward' -d 3 -m ./data2/shooting_momemta.vtk -o ./data2/q1_gpu.vtk -s 2.0 -l 5000 -n 40 -i 240 0
+'./shoot_forward' -d 3 -m ./data/shooting_momemta.vtk -o ./data/q1_gpu.vtk -s 2.0 -l 5000 -n 40 -i 240 0
 
